134775e0c5a14f88d2891176d4176bb3f800521f
Medwed-ops <70763550+Medwed-ops@users.noreply.github.com>
2020-11-30 14:48:13 +0300

    Create PR10_AbstractChairFactory.java

---
+++ PR10_AbstractChairFactory.java
@@ -0,0 +1,8 @@
+package PR10;
+
+public interface AbstractChairFactory {
+    VictorianChair createVictorianChair();
+    MagicChair createMagicChair();
+    FunctionalChair createFunctionalChair();
+
+}
